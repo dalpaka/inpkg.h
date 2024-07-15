95be8e466fe2f733c7bfe6fc19d805567b64ca82
dalpaka <dimitri.neu@gmail.com>
2024-07-15 22:41:05 +0200

    Update clrflow.h

--- src/clrflow.h
+++ src/clrflow.h
@@ -1,6 +1,20 @@
 #ifndef CLRFLOW_H
 #define CLRFLOW_H
+#include "string.h"
+#include "bool.h"
 
-extern int print(char[], char[]);
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define COLOR_RESET   "\x1b[0m"
+
+double print(char[] content, char[] color){
+    double fart = *content;
+
+    return 0;
+}
 
 #endif
